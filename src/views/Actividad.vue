e5a0333a8095c04ca8a343f89163e78af16b6fe7
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-11-18 13:59:16 -0500

    Update Actividad.vue

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,12 +10,12 @@
               img(src="@/assets/template/Seleccion_Multiple.svg", alt="Texto que describa la imagen")
         .col-12.col-md-8.col-lg-9
           .titulo-segundo
-            h2 Actividad didáctica 1
-          p.mb-4 Cras tempus tortor et nisi tincidunt feugiat. Integer lacus ante, venenatis nec fringilla quis, dapibus ut dui. Fusce imperdiet velit a leo sagittis, id scelerisque massa placerat. Duis at dui sed enim euismod pulvinar quis ac ante. Phasellus condimentum sapien nisi, ac facilisis purus tristique ac. Quisque magna est, luctus vitae elit ut, convallis venenatis justo. Nunc pretium elit a nisi tempus sagittis.
+            h2 Nombre de la Actividad
+          p.mb-4 Objetivo de la actividad
           .tarjeta.actividad.p-3
             .row.justify-content-around.align-items-center            
               .col-sm.mb-3.mb-sm-0
-                p.fw-bold.mb-0 Descripción de la actividad didáctica.
+                p.fw-bold.mb-0 Cuestionario
               .col-auto
                 a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                   span Realizar
