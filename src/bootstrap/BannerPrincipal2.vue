af995bf1d82a12d03e9a49660d5549eb8719dab8
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-11-17 22:26:04 -0500

    Update BannerPrincipal2.vue

--- src/bootstrap/BannerPrincipal2.vue
+++ src/bootstrap/BannerPrincipal2.vue
@@ -11,7 +11,7 @@
           p.mb-0(v-html="globalData.descripcionCurso")
         .banner-principal__accion
           router-link.boton(:to="{name: iniciarLnk.nombreRuta }")
-            span.me-1 Ver más 1
+            span.me-1 Ver más
             i.fas.fa-angle-right
 
       .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
